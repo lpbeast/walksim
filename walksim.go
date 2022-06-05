5b0f25ea33db50f814f30a4a82f55dbe835c1474
Mallory Cloutier <cloutier@eskimo.com>
2022-06-05 04:44:28 -0500

    multiword command parsing, part one

--- walksim.go
+++ walksim.go
@@ -17,6 +17,9 @@ func printhelp() {
 	fmt.Println("Available Commands")
 	fmt.Println("These commands can be abbreviated with the first letter of the word:")
 	fmt.Println("north, east, south, west, up, down, quit, help, look")
+	fmt.Println("These commands must be typed out in full:")
+	fmt.Println("put, drop, open, close")
+	fmt.Println("")
 }
 
 func main(){
@@ -24,6 +27,7 @@ func main(){
 	location := "101"
 	runloop := true
 	suppressdesc := false
+	var notacommand bool
 	
 	// read the rooms file and parse it out into the room list
 	
@@ -65,36 +69,42 @@ func main(){
 		}
 		fmt.Printf(">> ")
 		suppressdesc = false
+		notacommand = false
 		var uinput string
 		fmt.Scanln(&uinput)
+		uinputp := strings.Fields(uinput)
 		switch{
-			case uinput == "n":
-				uinput = "north"
-			case uinput == "e":
-				uinput = "east"
-			case uinput == "s":
-				uinput = "south"
-			case uinput == "w":
-				uinput = "west"
-			case uinput == "u":
-				uinput = "up"
-			case uinput == "d":
-				uinput = "down"
-			case uinput == "q":
-				uinput = "quit"
-			case uinput == "h":
-				uinput = "help"
-			case uinput == "l":
-				uinput = "look"
+			case uinputp[0] == "n":
+				uinputp[0] = "north"
+			case uinputp[0] == "e":
+				uinputp[0] = "east"
+			case uinputp[0] == "s":
+				uinputp[0] = "south"
+			case uinputp[0] == "w":
+				uinputp[0] = "west"
+			case uinputp[0] == "u":
+				uinputp[0] = "up"
+			case uinputp[0] == "d":
+				uinputp[0] = "down"
+			case uinputp[0] == "q":
+				uinputp[0] = "quit"
+			case uinputp[0] == "h":
+				uinputp[0] = "help"
+			case uinputp[0] == "l":
+				uinputp[0] = "look"
+			case uinputp[0] == "north" || uinputp[0] == "south" || uinputp[0] == "east" || uinputp[0] == "west":
+			case uinputp[0] == "up" || uinputp[0] == "down" || uinputp[0] == "help" || uinputp[0] == "quit" || uinputp[0] == "look":
+			case uinputp[0] == "get" || uinputp[0] == "drop" || uinputp[0] == "open" || uinputp[0] == "close":
 			default:
+				notacommand = true
 		}
 		switch{
-			case uinput == "quit":
+			case uinputp[0] == "quit":
 				runloop = false
-			case uinput == "help":
+			case uinputp[0] == "help":
 				printhelp()
 				suppressdesc = true
-			case uinput == "look":
+			case uinputp[0] == "look":
 				fmt.Println(roomlist[location].desc)
 				fmt.Printf("Exits:")
 				for key, _ := range roomlist[location].exits {
@@ -103,9 +113,13 @@ func main(){
 				fmt.Printf(".\n")
 				suppressdesc = true
 			default:
-				dest, ok := roomlist[location].exits[uinput]
+				dest, ok := roomlist[location].exits[uinputp[0]]
 				if ok == false {
-					fmt.Printf("You can't do '%v' here.\n", uinput)
+					if notacommand == true {
+						fmt.Printf("I don't understand what '%v' means here.\n", uinputp[0])
+					} else {
+						fmt.Printf("You can't go %v from here.\n", uinputp[0])
+					}
 					suppressdesc = true
 				} else {
 					location = dest
